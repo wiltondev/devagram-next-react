55cad36342a23ee6d385b514b9c76b87cc916582
wiltondev <92389600+wiltondev@users.noreply.github.com>
2024-01-07 16:46:35 -0300

    configuração

--- DevagramReactNext/componentes/layout/Rodape.js
+++
@@ -1,9 +0,0 @@
-import Navegacao from "./Navegacao";
-
-export default function Rodape() {
-    return (
-        <footer className="rodape mobile">
-            <Navegacao />
-        </footer>
-    );
-}
